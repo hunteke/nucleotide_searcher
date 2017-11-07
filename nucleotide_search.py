5d0f4e053a08bb9c35d6ce4617435bb2f98b3538
Kevin Hunter Kesling <hunteke@gmail.com>
2017-11-06 22:55:50 -0500

    Enable database name and identifier as CLI args.
     * Create URL with urlparse library, rather than simple regex/replace
     * Set defaults to the problem mentioned SARS coronavirus because it's a
       slim 30K to download.  If user forgets, won't have to wait long to
       find out
     * Implement basic argument validation from reading the Entrez API docs.[1]
       I did not find specific validation rules, but created these based on
       eyeballing what examples the docs did give.
    
    [1] https://www.ncbi.nlm.nih.gov/books/NBK25499/

--- nucleotide_search.py
+++ nucleotide_search.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
-import argparse
 import gzip
+import re
 import urllib.request
 
 def msg_and_die( msg ):
@@ -32,13 +32,55 @@ def read_url( url ):
 	return data
 
 
-parser = argparse.ArgumentParser(
-	description='Search for nucleotide sequences.'
-)
-parser.add_argument('--debug', action='store_true', help="Rather than the simplistic error messages, show developer-useful information (i.e., tracebacks).")
+def create_efetch_url( args ):
+	# Ostensibly performed more simply and quickly with a couple of regexes:
+	# not knowing what the hiring committee wants to see, nor the scope of what
+	# this "project" might entail beyond the "What can this candidate do?"
+	# question, I've opted for the more robust library approach.
+	url_parts = list( urllib.parse.urlparse( args.baseurl ))
+	query = urllib.parse.parse_qs( url_parts[ 4 ])
+	query.update({
+		'db': args.dbname,
+		'id': args.dbid,
+		'rettype': 'fasta',
+		'retmode': 'xml'
+	})
+	url_parts[4] = urllib.parse.urlencode( query )
+	return urllib.parse.urlunparse( url_parts )
+
 
 if '__main__' == __name__:
+	import argparse
+
+	def dbname_format( s, pat=re.compile(r'^[A-z]{1,16}$') ):
+		"""
+		From eyeballing all of the database names, a valid name must be an ASCII
+		alpha letter (A through Z), capitalized or not, and be no more than 16
+		characters lone.
+		"""
+		if not pat.match( s ):
+			raise argparse.ArgumentTypeError('Invalid database name.  (Single word, all letters, no more than 16 characters.)')
+		return s
+
+
+	def dbid_format( s, pat=re.compile(r'^\d+(?:,\d+)*') ):
+		"""
+		From the documentation, the strong inference is that database IDs are
+		numerical; more than one may be specified with a comma.
+		"""
+		if not pat.match( s ):
+			raise argparse.ArgumentTypeError('Invalid database ID(s).  (Database IDs are numerical; multiple IDs may be separated by a comma.)')
+		return s
+
+
+	parser = argparse.ArgumentParser(
+		description='Search for nucleotide sequences.'
+	)
+	parser.add_argument('--baseurl', default='http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi', help='The base URL from which to fetch data.')
+	parser.add_argument('--dbname', default="nucleotide", type=dbname_format, help="NCBI database name. [nucleotide]")
+	parser.add_argument('--dbid',   default="30271926", type=dbid_format, help="NCBI database identifier. [30271926]")
+	parser.add_argument('--debug', action='store_true', help="Rather than the simplistic error messages, show developer-useful information (i.e., tracebacks).")
 	args = parser.parse_args()
 
-	url = 'http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nucleotide&id=30271926&rettype=fasta&retmode=xml'
+	url = create_efetch_url( args )
 	print( read_url( url ))
