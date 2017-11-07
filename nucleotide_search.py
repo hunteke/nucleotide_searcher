98f0d9e4c92db2c9aad864f8b5630794401e98eb
Kevin Hunter Kesling <hunteke@gmail.com>
2017-11-06 22:40:18 -0500

    Argument parsing; "friendly" error messages
    I'm not entirely sure what the problem statement is going for with
    "reasonably concise messages sent to stderr".  I got the sense from
    Chaim that unless I was stuck, for this project, I should muddle through
    without questions.  I hope this threads the needle.

--- nucleotide_search.py
+++ nucleotide_search.py
@@ -1,12 +1,35 @@
 #!/usr/bin/env python3
 
+import argparse
 import urllib.request
+
+def msg_and_die( msg ):
+	if args.debug:
+		raise
+
+	raise SystemExit( "{}\n\nUse --debug for developer friendly information\n".format(msg) )
+
 
 def read_url( url ):
 	req = urllib.request.Request( url )
-	res = urllib.request.urlopen( req )
+
+	try:
+		res = urllib.request.urlopen( req )
+	except urllib.request.HTTPError as e:
+		msg_and_die( 'Attempted url: {}\nHTTP error reading data from server.\nLibrary error message: {}'.format(url, e) )
+	except urllib.error.URLError as e:
+		msg_and_die( 'Attempted url: {}\nUnable to make connection; check your internet connection?\nLibrary error message: {}'.format(url, e) )
+
 	return res.read()
 
+
+parser = argparse.ArgumentParser(
+	description='Search for nucleotide sequences.'
+)
+parser.add_argument('--debug', action='store_true', help="Rather than the simplistic error messages, show developer-useful information (i.e., tracebacks).")
+
 if '__main__' == __name__:
+	args = parser.parse_args()
+
 	url = 'http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nucleotide&id=30271926&rettype=fasta&retmode=xml'
 	print( read_url( url ))
